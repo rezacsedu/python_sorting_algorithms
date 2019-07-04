cb0889f98c6c82222070ba9a93e44032af70c2c2
Tu Vo <37724651+tuvo1106@users.noreply.github.com>
2019-07-04 09:50:18 -0700

    Update countsort.py

--- countsort.py
+++ countsort.py
@@ -1,13 +1,13 @@
-def countsort(l:list) -> list:
+def countsort(l:list):
     arr = l[::]
     k = 0
-    l = (max(arr) + 1) * [0]
+    new_list = (max(arr) + 1) * [0]
     for j in arr:
-        l[j] += 1
+        new_list[j] += 1
     for i in range(max(arr) + 1):
         if i != 0:
-            l[i] += l[i - 1]
-        while l[i] != k:
+            new_list[i] += new_list[i - 1]
+        while new_list[i] != k:
             arr[k] = i;
             k += 1
     return arr
